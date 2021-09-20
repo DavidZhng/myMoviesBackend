374c1ccdcdaa6538ef89e667abdca4c27c60b06e
DiiZyy <zhangdavid33@gmail.com>
2021-09-20 15:14:30 -0400

    go

--- middleware/requireSignin.js
+++ middleware/requireSignin.js
@@ -6,14 +6,14 @@ let User = require('../models/user.model');
 module.exports = (req, res, next) => {
     const {authorization} = req.headers
     if (!authorization) {
-        return res.status(401).json({error: "must be logged in"})
+        return res.json({error: "must be logged in"})
     }
     const token = authorization.split(' ')[1];
     console.log(token)
     jwt.verify(token, JWT_SECRET, (err, payload) => {
         console.log(token == JWT_SECRET)
         if (err) {
-            return res.status(401).json({error: "must be logged in"})
+            return res.json({error: "must be logged in"})
         }
         const {_id} = payload
         User.findById(_id).then(userData => {
